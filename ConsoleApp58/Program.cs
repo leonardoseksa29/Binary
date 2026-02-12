ad57c6321a6afc379a80cc366c4451eeae096e1a
mashinata123 <bozhidar.petrov.pp26@pgmtplovdiv.info>
2026-02-12 10:05:49 +0200

    Add project files.

---
+++ ConsoleApp58/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp58
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("въведете десетично число: ");
+        }   int n = int.Parse(Console.ReadLine());
+    }       string binary = "";
+}           while (n > 0);
+        {
+
+
+        }
