0ab08397ba993806bdae023a21b067b8c0b1ab22
mashinata123 <bozhidar.petrov.pp26@pgmtplovdiv.info>
2026-02-12 10:31:15 +0200

    New text file

--- ConsoleApp58/Program.cs
+++ ConsoleApp58/Program.cs
@@ -1,14 +1,18 @@
-﻿namespace ConsoleApp58
+﻿namespace BroyniSystemi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("въведете десетично число: ");
-        }   int n = int.Parse(Console.ReadLine());
-    }       string binary = "";
-}           while (n > 0);
-        {
-
-
+            Console.WriteLine("Въведете десетично число: ");
+            int n = int.Parse(Console.ReadLine());
+            string binary = "";
+            while (n > 0)
+            {
+                binary = (n % 2) + binary;
+                n /= 2;
+            }
+            Console.WriteLine($"Двоично: {binary}");
         }
+    }
+}
